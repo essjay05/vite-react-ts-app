5b00fd02f13b8cc9e01990399575dfc587e2b349
Joy Serquiña <stepjoyous@gmail.com>
2023-01-08 17:56:07 -0800

    Added dependency jsdom, updated App to have h1 containing 'Hello World', successfully set up and run basic App.tsx test.

---
+++ src/App.test.tsx
@@ -0,0 +1,18 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  it('Renders hello world', () => {
+    // ARRANGE
+    render(<App />);
+    // ACT
+    // EXPECTS
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Hello World');
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 function App() {
   return (
-    <form>
-      <input
-        id="dinosaur_name"
-        name="dinosaur_name"
-        className="dinosaur-input"
-        aria-describedby="dinosaur_name_info"
-      />
-    </form>
+    <>
+      <h1>Hello World</h1>
+      <form>
+        <input
+          id="dinosaur_name"
+          name="dinosaur_name"
+          className="dinosaur-input"
+          aria-describedby="dinosaur_name_info"
+        />
+      </form>
+    </>
   );
 }
 
